11bd07d7b7dee6f58866351d64e7ad3ca3445621
Oscar Martinez <ozcarr753@gmail.com>
2023-03-20 16:32:43 -0700

    Finalized app

--- SovosWebProject/Controllers/CategoryController.cs
+++ SovosWebProject/Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
 using SovosWebProject.Data;
 using SovosWebProject.Models;
+using System.ComponentModel;
 
 namespace SovosWebProject.Controllers
 {
@@ -17,6 +20,85 @@ namespace SovosWebProject.Controllers
         {
             IEnumerable<Category> objCategoryList = _dbContext.Categories;
             return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var category = _dbContext.Categories.Find(id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot be the same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["Success"] = "Category edited succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var category = _dbContext.Categories.Find(id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var category = _dbContext.Categories.Find(id);
+            if (category == null) return NotFound();
+            _dbContext.Remove(category);
+            _dbContext.SaveChanges();
+            TempData["Success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot be the same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                TempData["Success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
--- SovosWebProject/Models/Category.cs
+++ SovosWebProject/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace SovosWebProject.Models
@@ -9,6 +10,8 @@ namespace SovosWebProject.Models
         public int Id { get; set; }
         [Required, MaxLength(length: 100)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100,ErrorMessage ="Display order must be between 1 and 100!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
